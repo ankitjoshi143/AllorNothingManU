63bf1f1f0ea44ea53f071a599238d475446e7ed9
Ankit Joshi <ankitjoshi143@gmail.com>
2022-04-08 22:16:23 -0600

    resolved error in netlify

--- src/components/panels/AllProductsPanel.js
+++ src/components/panels/AllProductsPanel.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect}  from "react";
+import React  from "react";
 
 import { useGetProducts } from "hooks/useGetProducts";
 import {PanelStyles, PanelTitle, PanelBody} from './styles'

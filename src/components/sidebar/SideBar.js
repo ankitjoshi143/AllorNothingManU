b4d6f5d98cd7121aaa38ea24524b90acdb32dd89
Ankit Joshi <ankitjoshi143@gmail.com>
2022-04-08 22:17:36 -0600

    unused var removed

--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from 'react-router-dom'
 
 import ProductOptions from "./ProductOptions";
 import {SideBarStyles, SideBarTitle, SideBarButton, SideBarItems, SideBarItem} from './styles';
